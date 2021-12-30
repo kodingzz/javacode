257455c66f3c4fb69bf0ae5251799367572ed754
hw00 <khwland090@gmail.com>
2021-12-30 22:46:26 +0900

    java coding

---
+++ _ch04/src/_ch04/pr4_5.java
@@ -0,0 +1,15 @@
+package _ch04;
+
+public class pr4_5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		while(i<=10) {
+			for(int j=0;j<=i;j++)
+				System.out.print("*");
+			System.out.println();
+			i++;
+		}
+	}
+}
