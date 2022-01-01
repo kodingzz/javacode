e0b4f0d02c591ffeeb1a6989339f883bc6b5f384
hw00 <khwland090@gmail.com>
2022-01-01 13:41:37 +0900

    the output of the array

---
+++ array/src/array/ex5_1.java
@@ -0,0 +1,35 @@
+package array;
+
+import java.util.Arrays;
+
+public class ex5_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr= {100,95,96,97,98};
+		System.out.println(Arrays.toString(arr));        // ctrl+shift + o : import 문장 자동 생성
+		
+		int[] iarr1= new int[10];
+		int[] iarr2=new int[10];
+		int[] iarr3= {100,95,80,70,60};
+		char[] charr= {'a','b','c','d'};
+		
+		for(int i=0;i<iarr1.length;i++) 
+			iarr1[i]=i+1;
+		for(int i=0;i<iarr2.length;i++)
+			iarr2[i]=(int)(Math.random()*10)+1;
+		
+		for(int i=0;i<iarr1.length;i++)
+			System.out.printf("%d,",iarr1[i]);
+		
+		System.out.println();
+		
+		System.out.println(Arrays.toString(iarr2));   // Arrays.toString() ->[....] 출력
+		System.out.println(Arrays.toString(iarr3));
+		System.out.println(Arrays.toString(charr));
+		System.out.println(iarr3);      
+		System.out.println(charr);  // char형 배열은 내용 그대로 나온다 
+
+	}
+
+}
